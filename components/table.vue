9e5edf701920609c7ed04468dc851e916d6fd2b6
saumyagoyal1708 <1705071@kiit.ac.in>
2019-06-09 01:00:08 +0530

    table updated

--- components/table.vue
+++ components/table.vue
@@ -1,151 +1,160 @@
 <template>
 <div>
-	<v-container fluid>
-		
-		<v-tabs v-model="active" color="cyan" dark slider-color="yellow">
-	        <v-tab v-for="n in 3" :key="n" ripple>
-	        	Item {{ n }}
-    		</v-tab>
-        	<v-tab-item v-for="n in 3" :key="n" >
-          		<v-card flat>
-            		<v-card-text>{{ text }}</v-card-text>
-         		 </v-card>
-        	</v-tab-item>
-      	</v-tabs>
+  <v-container fluid>
+    
+    <v-tabs v-model="active" color="cyan" dark slider-color="yellow">
+          <v-tab v-for="n in 3" :key="n" ripple>
+            Item {{ n }}
+        </v-tab>
+          <v-tab-item v-for="n in 3" :key="n" >
+              <v-card flat>
+                <v-card-text>
+
+                  <br><br>
+                  <v-toolbar flat color = "lightgrey">
+        
+                  <v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details>
+                </v-text-field>
+                  <v-spacer></v-spacer>
+                  <v-dialog v-model="dialog" max-width="500px">
+                    <template v-slot:activator="{ on }">
+                        <v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
+                      </template>
+                      <v-card>    
+                        <v-card-text>
+                            <v-container grid-list-md>
+                              <v-layout wrap>
+                                  <v-flex xs12 sm6 md4>
+                                    <v-text-field v-model="editedItem.name" label="Serial No"></v-text-field>
+                                  </v-flex>
+                                  <v-flex xs12 sm6 md4>
+                                    <v-text-field v-model="editedItem.code" label="Code"></v-text-field>
+                                  </v-flex>
+                                  <v-flex xs12 sm6 md4>
+                                    <v-text-field v-model="editedItem.subject" label="Subject"></v-text-field>
+                                  </v-flex>
+                                  <v-flex xs12 sm6 md4>
+                                    <v-select :items="items" v-model="editedItem.teacher" label="teacher" solo full-width flat> </v-select>
+
+                                  </v-flex>
+                                  <v-flex xs12 sm6 md4>
+                                    <v-text-field v-model="editedItem.stream" label="Stream"></v-text-field>
+                                  </v-flex>
+                                  <v-flex xs12 sm6 md4>
+                                    <!--<v-text-field v-model="editedItem.topic" label="Topic"></v-text-field>-->
+
+
+
+
+
+                                    <v-dialog mv-model="dialog" width="500" >
+                                <template v-slot:activator="{ on }" >
+                                  <v-btn  v-on="on" >
+                                      Topics
+                                  </v-btn>
+                                </template>
+        
+                                <v-card>
+                                <v-card-actions>
+                                      <v-spacer></v-spacer>
+                                      <v-btn color="primary" flat @click="dialog = false">
+                                        <v-icon>close</v-icon>
+                                      </v-btn>
+                                  </v-card-actions>
+                                  <v-card-text>
+                                    <v-chip>topic</v-chip>
+                                    <v-chip>topic</v-chip>
+                                    <v-chip>topic</v-chip>
+                                    <v-chip>topic</v-chip>
+                                  </v-card-text>
+                                </v-card>
+                            </v-dialog>
+
+
+
+
+
+                                  </v-flex>
+                              </v-layout>
+                            </v-container>
+                        </v-card-text>
+            
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
+                            <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+                </v-toolbar>
+
+
+          <v-data-table :headers="headers" :items="Serial_No" class="elevation-1" >
+
+                <template v-slot:items="props">
+                  <td>{{ props.item.name }}</td>
+                  <td class="text-xs">{{ props.item.code }} </td>
+                  <td class="text-xs">{{ props.item.subject }}</td>
+                  <td class="text-xs">{{ props.item.teacher }}</td>
+                  <td class="text-xs">{{ props.item.stream }}</td>
+                  <td class="text-xs">{{ props.item.topic }} &emsp;&emsp; 
+
+                    <v-dialog mv-model="dialog" width="500" >
+                      <template v-slot:activator="{ on }">
+                        <v-btn  v-on="on" >
+                             View All
+                         </v-btn>
+                      </template>
+        
+                      <v-card>
+                
+                      <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="primary" flat @click="dialog = false">
+                              <v-icon>close</v-icon>
+                            </v-btn>
+                        </v-card-actions>
+                        <v-card-text>
+                          <v-chip>topic</v-chip>
+                          <v-chip>topic</v-chip>
+                          <v-chip>topic</v-chip>
+                          <v-chip>topic</v-chip>
+                        </v-card-text>
+
+                      </v-card>
+                   </v-dialog>
+
+                  </td>
+                  <td class="justify-center layout px-0">
+                  <v-icon small class="mr-2" @click="editItem(props.item)" >
+                    edit
+                  </v-icon>
+                  <v-icon small @click="deleteItem(props.item)">
+                    delete
+                  </v-icon>
+                </td>
+                  
+                </template>
+              </v-data-table>
+
+              <br>
+
+
+
+                </v-card-text>
+             </v-card>
+          </v-tab-item>
+        </v-tabs>
   
-      	<div class="text-xs-center mt-3">
-         	<v-btn @click="next">next tab</v-btn>
+        <div class="text-xs-center mt-3">
+          <v-btn @click="next">next tab</v-btn>
          </div>
          <br><br><br>
 
-			<v-toolbar flat color = "lightgrey">
-        
-		        <v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details>
-		   		</v-text-field>
-		        <v-spacer></v-spacer>
-		        <v-dialog v-model="dialog" max-width="500px">
-		        	<template v-slot:activator="{ on }">
-		            	<v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
-		          	</template>
-		          	<v-card>	  
-		            	<v-card-text>
-		              		<v-container grid-list-md>
-	                			<v-layout wrap>
-		                  			<v-flex xs12 sm6 md4>
-		                    			<v-text-field v-model="editedItem.name" label="Serial No"></v-text-field>
-		                  			</v-flex>
-		                  			<v-flex xs12 sm6 md4>
-		                   				<v-text-field v-model="editedItem.code" label="Code"></v-text-field>
-		                  			</v-flex>
-                  					<v-flex xs12 sm6 md4>
-	                    				<v-text-field v-model="editedItem.subject" label="Subject"></v-text-field>
-		                  			</v-flex>
-		                  			<v-flex xs12 sm6 md4>
-		                    			<v-select :items="items" v-model="editedItem.teacher" label="teacher" solo full-width flat> </v-select>
-
-		                  			</v-flex>
-		                  			<v-flex xs12 sm6 md4>
-		                    			<v-text-field v-model="editedItem.stream" label="Stream"></v-text-field>
-		                  			</v-flex>
-		                  			<v-flex xs12 sm6 md4>
-		                    			<!--<v-text-field v-model="editedItem.topic" label="Topic"></v-text-field>-->
-
-
-
-
-
-		                    			<v-dialog mv-model="dialog" width="500" >
-        									<template v-slot:activator="{ on }" >
-         										<v-btn  v-on="on" >
-           						 					Topics
-         					 					</v-btn>
-        									</template>
-  
-        									<v-card>
-				  								<v-card-actions>
-					            					<v-spacer></v-spacer>
-					            					<v-btn color="primary" flat @click="dialog = false">
-					            						<v-icon>close</v-icon>
-					            					</v-btn>
-					        					</v-card-actions>
-					        					<v-card-text>
-										        	<v-chip>topic</v-chip>
-										        	<v-chip>topic</v-chip>
-										        	<v-chip>topic</v-chip>
-										        	<v-chip>topic</v-chip>
-				        						</v-card-text>
-        									</v-card>
-     				 					</v-dialog>
-
-
-
-
-
-		                  			</v-flex>
-		                		</v-layout>
-		              		</v-container>
-		            	</v-card-text>
-		  
-			            <v-card-actions>
-			              	<v-spacer></v-spacer>
-			              	<v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
-			              	<v-btn color="blue darken-1" flat @click="save">Save</v-btn>
-			            </v-card-actions>
-	        		</v-card>
-		        </v-dialog>
-	        </v-toolbar>
-
-
-		<v-data-table :headers="headers" :items="Serial_No" class="elevation-1" >
-
-	        <template v-slot:items="props">
-		        <td>{{ props.item.name }}</td>
-		        <td class="text-xs">{{ props.item.code }} </td>
-		        <td class="text-xs">{{ props.item.subject }}</td>
-		        <td class="text-xs">{{ props.item.teacher }}</td>
-		        <td class="text-xs">{{ props.item.stream }}</td>
-		        <td class="text-xs">{{ props.item.topic }} &emsp;&emsp; 
-
-		        	<v-dialog mv-model="dialog" width="500" >
-        				<template v-slot:activator="{ on }">
-         					<v-btn  v-on="on" >
-           						 View All
-         					 </v-btn>
-        				</template>
-  
-        				<v-card>
-          
-					  		<v-card-actions>
-					            <v-spacer></v-spacer>
-					            <v-btn color="primary" flat @click="dialog = false">
-					            	<v-icon>close</v-icon>
-					            </v-btn>
-					        </v-card-actions>
-					        <v-card-text>
-					        	<v-chip>topic</v-chip>
-					        	<v-chip>topic</v-chip>
-					        	<v-chip>topic</v-chip>
-					        	<v-chip>topic</v-chip>
-					        </v-card-text>
-
-        				</v-card>
-     				 </v-dialog>
-
-		        </td>
-		        <td class="justify-center layout px-0">
-            <v-icon small class="mr-2" @click="editItem(props.item)" >
-              edit
-            </v-icon>
-            <v-icon small @click="deleteItem(props.item)">
-              delete
-            </v-icon>
-          </td>
-		        
-	        </template>
-        </v-data-table>
+      
 
         
-	</v-container>
+  </v-container>
 </div>
 </template>
 
@@ -208,7 +217,7 @@ export default{
   methods: {
     initialize () {
       this.Serial_No = [
-    	{
+      {
           name: '1',
           code : '--',
           subject: '--',
@@ -244,15 +253,15 @@ export default{
           topic: '24'
           
         },
-	     {
-	      name: '5',
-	      code : '--',
-	      subject: '--',
-	      teacher: '--',
-	      stream: '--',
-	      topic: '24'
-	      
-	    }
+       {
+        name: '5',
+        code : '--',
+        subject: '--',
+        teacher: '--',
+        stream: '--',
+        topic: '24'
+        
+      }
        
       ]
     },
@@ -298,21 +307,13 @@ export default{
 <style scoped>
 .place
 {
-	height : 56px;
+  height : 56px;
 }
 
 .btn
 {
-	margin-left : 400px ;
+  margin-left : 400px ;
 
 }
 </style>
-
-
-
-
-
-
-
-
 
