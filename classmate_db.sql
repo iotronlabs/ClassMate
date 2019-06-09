5576d56ebd9e840f3fae7ee346031a098bc9925d
Anand MAurya <1705018@kiit.ac.in>
2019-06-09 18:29:19 +0530

    db update

--- classmate_db.sql
+++ classmate_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 24, 2019 at 08:52 AM
+-- Generation Time: Jun 09, 2019 at 02:57 PM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -25,6 +25,20 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `channels`
+--
+
+CREATE TABLE `channels` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `migrations`
 --
 
@@ -40,7 +54,19 @@ CREATE TABLE `migrations` (
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1);
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_06_04_172032_create_threads_table', 1),
+(4, '2019_06_04_172338_create_replies_table', 1),
+(5, '2019_06_04_172500_create_channels_table', 1),
+(6, '2019_06_04_172631_create_saves_table', 1),
+(7, '2019_06_05_050114_create_user_admins_table', 1),
+(8, '2019_06_05_102850_create_user_students_table', 1),
+(9, '2019_06_05_103015_create_user_teachers_table', 1),
+(10, '2019_06_08_051750_create_user_staffs_table', 1),
+(11, '2019_06_08_061720_create_table_guardians_table', 1),
+(12, '2019_06_08_062021_create_table_classes_table', 1),
+(13, '2019_06_08_062712_create_table_courses_table', 1),
+(14, '2019_06_08_134336_create_foreign_keys', 1);
 
 -- --------------------------------------------------------
 
@@ -52,6 +78,120 @@ CREATE TABLE `password_resets` (
   `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `replies`
+--
+
+CREATE TABLE `replies` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `thread_id` int(10) UNSIGNED NOT NULL,
+  `body` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `saves`
+--
+
+CREATE TABLE `saves` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `saved_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_classes`
+--
+
+CREATE TABLE `table_classes` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `u_id` int(10) UNSIGNED NOT NULL,
+  `t_ref_id` int(11) NOT NULL,
+  `standard` int(10) UNSIGNED NOT NULL,
+  `section` char(1) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ct_id` int(10) UNSIGNED NOT NULL,
+  `course_id` int(10) UNSIGNED NOT NULL,
+  `count` int(11) NOT NULL,
+  `status` tinyint(3) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_courses`
+--
+
+CREATE TABLE `table_courses` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `u_id` int(10) UNSIGNED NOT NULL,
+  `t_ref_id` int(11) NOT NULL,
+  `c_id` int(10) UNSIGNED NOT NULL,
+  `t_id` int(10) UNSIGNED NOT NULL,
+  `s_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `c_day` int(11) NOT NULL,
+  `time_start` time NOT NULL,
+  `time_end` time NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_guardians`
+--
+
+CREATE TABLE `table_guardians` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `u_id` int(10) UNSIGNED NOT NULL,
+  `t_ref_id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gender` char(1) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contact` int(10) UNSIGNED NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `table_guardians`
+--
+
+INSERT INTO `table_guardians` (`id`, `u_id`, `t_ref_id`, `name`, `email`, `gender`, `contact`, `password`, `status`, `created_at`, `updated_at`) VALUES
+(1, 12, 32, 'anand', '1705018@kiit.ac.in', 'F', 123456789, '1233456789', 111, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `threads`
+--
+
+CREATE TABLE `threads` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `channel_id` int(10) UNSIGNED NOT NULL,
+  `replies_count` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `title` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `body` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -71,17 +211,100 @@ CREATE TABLE `users` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `users`
+-- Table structure for table `user_admins`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(9, '1234Anand', '4567@tdsting.com', NULL, '$2y$10$ClGcaDtokMWM2Ch5oZrZNOYBQpSCeMPedkH2zASdRmN/8dzr7P616', NULL, '2019-05-24 00:53:38', '2019-05-24 00:53:38'),
-(10, 'Anand Maurya', 'iotron@gmail.com', NULL, '$2y$10$vc/fHdvEOcvVA/6rWZFUj.qA4hZJnTXH1kRD.PeIo3cFTX9nNDuBW', NULL, '2019-05-24 01:21:27', '2019-05-24 01:21:27');
+CREATE TABLE `user_admins` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `u_id` int(11) NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gender` char(1) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contact` int(10) UNSIGNED NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_staffs`
+--
+
+CREATE TABLE `user_staffs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `u_id` int(10) UNSIGNED NOT NULL,
+  `t_ref_id` int(11) NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gender` char(1) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contact` int(10) UNSIGNED NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` tinyint(3) UNSIGNED NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_students`
+--
+
+CREATE TABLE `user_students` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `u_id` int(10) UNSIGNED NOT NULL,
+  `t_ref_id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gender` char(1) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contact` int(10) UNSIGNED NOT NULL,
+  `d_o_b` date NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `guardian_id` int(10) UNSIGNED NOT NULL,
+  `class_id` int(10) UNSIGNED NOT NULL,
+  `status` tinyint(3) UNSIGNED NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_teachers`
+--
+
+CREATE TABLE `user_teachers` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `u_id` int(10) UNSIGNED NOT NULL,
+  `t_ref_id` int(11) NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gender` char(1) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contact` int(10) UNSIGNED NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` tinyint(3) UNSIGNED NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `channels`
+--
+ALTER TABLE `channels`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `migrations`
@@ -96,6 +319,51 @@ ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
 
 --
+-- Indexes for table `replies`
+--
+ALTER TABLE `replies`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `saves`
+--
+ALTER TABLE `saves`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `saves_user_id_saved_id_unique` (`user_id`,`saved_id`);
+
+--
+-- Indexes for table `table_classes`
+--
+ALTER TABLE `table_classes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `table_classes_u_id_unique` (`u_id`),
+  ADD KEY `table_classes_ct_id_foreign` (`ct_id`),
+  ADD KEY `table_classes_course_id_foreign` (`course_id`);
+
+--
+-- Indexes for table `table_courses`
+--
+ALTER TABLE `table_courses`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `table_courses_u_id_unique` (`u_id`),
+  ADD KEY `table_courses_t_id_foreign` (`t_id`),
+  ADD KEY `table_courses_c_id_foreign` (`c_id`);
+
+--
+-- Indexes for table `table_guardians`
+--
+ALTER TABLE `table_guardians`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `table_guardians_u_id_unique` (`u_id`),
+  ADD UNIQUE KEY `table_guardians_email_unique` (`email`);
+
+--
+-- Indexes for table `threads`
+--
+ALTER TABLE `threads`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -103,20 +371,145 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `users_email_unique` (`email`);
 
 --
+-- Indexes for table `user_admins`
+--
+ALTER TABLE `user_admins`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_admins_u_id_unique` (`u_id`),
+  ADD UNIQUE KEY `user_admins_email_unique` (`email`);
+
+--
+-- Indexes for table `user_staffs`
+--
+ALTER TABLE `user_staffs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_staffs_u_id_unique` (`u_id`),
+  ADD UNIQUE KEY `user_staffs_email_unique` (`email`);
+
+--
+-- Indexes for table `user_students`
+--
+ALTER TABLE `user_students`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_students_u_id_unique` (`u_id`),
+  ADD UNIQUE KEY `user_students_email_unique` (`email`),
+  ADD KEY `user_students_guardian_id_foreign` (`guardian_id`),
+  ADD KEY `user_students_class_id_foreign` (`class_id`);
+
+--
+-- Indexes for table `user_teachers`
+--
+ALTER TABLE `user_teachers`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_teachers_u_id_unique` (`u_id`),
+  ADD UNIQUE KEY `user_teachers_email_unique` (`email`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `channels`
+--
+ALTER TABLE `channels`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `replies`
+--
+ALTER TABLE `replies`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `saves`
+--
+ALTER TABLE `saves`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `table_classes`
+--
+ALTER TABLE `table_classes`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `table_courses`
+--
+ALTER TABLE `table_courses`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `table_guardians`
+--
+ALTER TABLE `table_guardians`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `threads`
+--
+ALTER TABLE `threads`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_admins`
+--
+ALTER TABLE `user_admins`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_staffs`
+--
+ALTER TABLE `user_staffs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_students`
+--
+ALTER TABLE `user_students`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_teachers`
+--
+ALTER TABLE `user_teachers`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `table_classes`
+--
+ALTER TABLE `table_classes`
+  ADD CONSTRAINT `table_classes_course_id_foreign` FOREIGN KEY (`course_id`) REFERENCES `table_courses` (`u_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `table_classes_ct_id_foreign` FOREIGN KEY (`ct_id`) REFERENCES `user_teachers` (`u_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `table_courses`
+--
+ALTER TABLE `table_courses`
+  ADD CONSTRAINT `table_courses_c_id_foreign` FOREIGN KEY (`c_id`) REFERENCES `table_classes` (`u_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `table_courses_t_id_foreign` FOREIGN KEY (`t_id`) REFERENCES `user_teachers` (`u_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `user_students`
+--
+ALTER TABLE `user_students`
+  ADD CONSTRAINT `user_students_class_id_foreign` FOREIGN KEY (`class_id`) REFERENCES `table_classes` (`u_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `user_students_guardian_id_foreign` FOREIGN KEY (`guardian_id`) REFERENCES `table_guardians` (`u_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
